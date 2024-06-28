11c0ec85b2eb474742bef811f8037d3b5a07d169
Dewey <deweyfang@gmail.com>
2024-06-27 22:37:29 -0700

    Update i18n

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -26,7 +26,7 @@ const config: Config = {
   // may want to replace "en" with "zh-Hans".
   i18n: {
     defaultLocale: 'en',
-    locales: ['en'],
+    locales: ['en', 'zh'],
   },
 
   presets: [
@@ -65,7 +65,10 @@ const config: Config = {
           position: 'left',
           label: 'Tutorial',
         },
-        {to: '/blog', label: 'Blog', position: 'left'},
+        {
+          type: 'localeDropdown',
+          position: 'right',
+        },
         {
           href: 'https://github.com/facebook/docusaurus',
           label: 'GitHub',
